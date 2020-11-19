0c78b0523ebc53180cc57f39cf9718e2057abda2
63070014 <73680260+63070014@users.noreply.github.com>
2020-11-19 20:22:46 +0700

    Update add.php

--- add.php
+++ add.php
@@ -1,3 +1,7 @@
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'datadata.mysql.database.azure.com', 'datadata@datadata', 'PuiFai2023', 'ITFLab', 3306);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,50 +12,46 @@
     <link rel="stlylesheet" href="bootstrap/bootstrap.css">
 </head>
 <body>
-<?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'datadata.mysql.database.azure.com', 'datadata@datadata', 'PuiFai2023', 'ITFLab', 3306);
-if (isset($errorMsg)) {
-?>
-    <div class="alert alert-sucess">
-        <strong>Wrong! <?php echo $errorMsg; ?> </strong>
-    </div>
-<?php 
-}
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
-?>
-<?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'datadata.mysql.database.azure.com', 'datadata@datadata', 'PuiFai2023', 'ITFLab', 3306);
-if (isset($insertMsg)) {
-?>
-    <div class="alert alert-sucess">
-        <strong>Sucess! <?php echo $insertMsg; ?> </strong>
-    </div>
-<?php 
-}
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
-?>
-<form method="post" class="form-horizontal">
-    <div class+'form-group'>
-        <label for="Name" class='col-sm-3 control-label'>Name</label>
-        <div class='col-sm-6'>
-            <input type='text' name='txt_name' class='form-control' placeholder='Enter Name'></input>
+
+    <div class='container'>
+    <div class='display-3 text-center'>Add+</div>
+
+    <?php
+        if (isset($errorMsg)) {
+    ?>
+        <div class="alert alert-danger">
+            <strong>Wrong! <?php echo $errorMsg; ?> </strong>
         </div>
-    </div>
-    <div class+'form-group'>
-        <label for="Comment" class='col-sm-3 control-label'>Comment</label>
-        <div class='col-sm-6'>
-            <input type='text' name='txt_comment' class='form-control' placeholder='Enter Comment'></input>
+    <?php } ?>
+
+    <?php
+        if (isset($insertMsg)) {
+    ?>
+        <div class="alert alert-success">
+            <strong>Succes! <?php echo $imsertMsg; ?> </strong>
         </div>
-    </div>
-    <div class+'form-group'>
-        <div class='col-sm-offset-3 col-sm-9 mt-5'>
-            <input type='Submit' name='btn_insert' class='btn btn-success' value='Insert'></input>
-            <a href='show.php' class='btn btn-danger'>Cancel</a>
+    <?php } ?>
+
+    <form method="post" class="form-horizontal">
+        <div class+'form-group'>
+            <label for="Name" class='col-sm-3 control-label'>Name</label>
+            <div class='col-sm-6'>
+                <input type='text' name='txt_name' class='form-control' placeholder='Enter Name'></input>
+            </div>
         </div>
-    </div>
-</form>
+        <div class+'form-group'>
+            <label for="Comment" class='col-sm-3 control-label'>Comment</label>
+            <div class='col-sm-6'>
+                <input type='text' name='txt_comment' class='form-control' placeholder='Enter Comment'></input>
+            </div>
+        </div>
+        <div class+'form-group'>
+            <div class='col-sm-offset-3 col-sm-9 mt-5'>
+                <input type='Submit' name='btn_insert' class='btn btn-success' value='Insert'></input>
+                <a href='show.php' class='btn btn-danger'>Cancel</a>
+            </div>
+        </div>
+    </form>
 
     <script src="js/slim.js"></script>
     <script src="js/popper.js"></script>
