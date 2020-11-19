3af1ff174670cfa757f8214bf894ae1a18357d50
63070014 <73680260+63070014@users.noreply.github.com>
2020-11-19 20:42:15 +0700

    Update add.php

--- add.php
+++ add.php
@@ -7,7 +7,7 @@ mysqli_real_connect($conn, 'datadata.mysql.database.azure.com', 'datadata@datada
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit</title>
+    <title>Add</title>
 
     <link rel="stlylesheet" href="bootstrap/bootstrap.css">
 </head>
@@ -33,24 +33,24 @@ mysqli_real_connect($conn, 'datadata.mysql.database.azure.com', 'datadata@datada
     <?php } ?>
 
     <form method="post" class="form-horizontal">
-        <div class='form-group'>
-            <label for="Name" class='col-sm-3 control-label'>Name</label>
-            <div class='col-sm-6'>
-                <input type='text' name='txt_name' class='form-control' placeholder='Enter Name'></input>
+            <div class='form-group'>
+                <label for="Name" class='col-sm-3 control-label'>Name</label>
+                <div class='col-sm-6'>
+                    <input type='text' name='txt_name' class='form-control' placeholder='Enter Name'></input>
+                </div>
             </div>
-        </div>
-        <div class='form-group'>
-            <label for="Comment" class='col-sm-3 control-label'>Comment</label>
-            <div class='col-sm-6'>
-                <input type='text' name='txt_comment' class='form-control' placeholder='Enter Comment'></input>
+            <div class='form-group'>
+                <label for="Comment" class='col-sm-3 control-label'>Comment</label>
+                <div class='col-sm-6'>
+                    <input type='text' name='txt_comment' class='form-control' placeholder='Enter Comment'></input>
+                </div>
             </div>
-        </div>
-        <div class='form-group'>
-            <div class='col-sm-offset-3 col-sm-9 mt-5'>
-                <input type='Submit' name='btn_insert' class='btn btn-success' value='Insert'></input>
-                <a href='show.php' class='btn btn-danger'>Cancel</a>
+            <div class='form-group'>
+                <div class='col-sm-offset-3 col-sm-9 mt-5'>
+                    <input type='Submit' name='btn_insert' class='btn btn-success' value='Insert'></input>
+                    <a href='show.php' class='btn btn-danger'>Cancel</a>
+                </div>
             </div>
-        </div>
     </form>
 
     <script src="js/slim.js"></script>
